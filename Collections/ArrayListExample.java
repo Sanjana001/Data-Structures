dd659e91acb04139a49821f08d92eef8cb368dd3
Sanjana001 <60166995+Sanjana001@users.noreply.github.com>
2021-02-18 16:58:21 +0530

    ArrayList in java

--- Collections/ArrayListExample.java
+++ Collections/ArrayListExample.java
@@ -2,13 +2,13 @@ import java.util.*;
 class ArrayListExample{
 	public static void main(String[] args){
 		ArrayList<Integer> arrayList = new ArrayList<Integer>();
-	    //add elements in arrayList
-	    arrayList.add(10);
-	    arrayList.add(2);
-	    arrayList.add(3);
-	    System.out.println("Elements inside arrayList are: "+arrayList);
-	    //add elements at a particular position
-	    arrayList.add(1,12);
+	        //add elements in arrayList
+	        arrayList.add(10);
+	        arrayList.add(2);
+	        arrayList.add(3);
+	        System.out.println("Elements inside arrayList are: "+arrayList);
+	        //add elements at a particular position
+	        arrayList.add(1,12);
 		arrayList.add(4,23);
 		arrayList.add(0,11);
 		System.out.println("\nAfter adding elements at some particular position the resultant arrayList is "+arrayList);
@@ -27,6 +27,7 @@ class ArrayListExample{
 		System.out.println("\nSorted arrayList is "+arrayList);
 		//remove the second element from arrayList
 		arrayList.remove(1);
+		//we can use use remove(index) or remove(element) like If I want to remove 50 then I can use remove(5) or remove(50)
 		System.out.println("\nAfter removing the second element the resultant arrayList is "+arrayList);
 		System.out.println("\nIs 2 present in arrayList? "+arrayList.contains(2));
 		System.out.println("\nConvert arrayList into array "+arrayList.toArray());
