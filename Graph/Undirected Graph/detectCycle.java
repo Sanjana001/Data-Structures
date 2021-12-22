99d32539c2b56a58bbce7a33de71e67c06d5df1f
Sanjana001 <sanjana0056.cse19@chitkara.edu.in>
2021-12-22 19:16:12 +0530

    codes

---
+++ Graph/Undirected Graph/detectCycle.java
@@ -0,0 +1,85 @@
+import java.util.*;
+
+class Node{
+	int parent, vertex;
+	Node( int vertex, int parent ){
+		this.vertex = vertex;
+		this.parent = parent;
+	}
+}
+
+class Graph{
+	int vertices; // no of vertices
+	boolean[][] adj_matrix; // adjacency matrix
+	
+	Graph( int vertices ){
+		this.vertices = vertices;
+		this.adj_matrix = new boolean[this.vertices][this.vertices];
+	}
+	
+	// add edge to the graph
+	void addEdge( int u, int v ) {
+		this.adj_matrix[u][v] = true;
+		this.adj_matrix[v][u] = true;
+	}
+	
+	// detect cycle in graph
+	void detectCycle() {
+		boolean[] visited = new boolean[this.vertices];
+		Arrays.fill(visited, false);
+		// traverse the graph through each vertex
+		// as it might be possible that the given graph is a disconnected one
+		// or the graph whose all components are not connected to others
+		for( int i=0;i<this.vertices;i++ ) {
+			if( visited[i] != true ) {
+				if( bfsUtil( visited, i ) ) {
+					System.out.println( "Cycle Exists" );
+					return ;
+				}
+			}
+		}
+		System.out.println( "Cycle Doesn't Exist" );
+	}
+	
+	// apply the breadth first search algorithm
+	boolean bfsUtil( boolean[] visited, int vertex ) {
+		Queue<Node> queue = new LinkedList<Node>();
+		// let's assume that the parent of the root node is -1
+		queue.add( new Node(vertex,-1) );
+		visited[vertex] = true;
+		// run the loop until the size of queue becomes zero
+		while( queue.isEmpty() != true ) {
+			Node element = queue.remove();
+			for( int i=0;i<this.vertices;i++ ) {
+				if( this.adj_matrix[element.vertex][i] ) {
+					if( visited[i] ) {
+						if( i != element.parent ) return true;
+					} else {
+						queue.add( new Node(i,element.vertex) );
+						visited[i] = true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+}
+
+public class detectCycle {
+	public static void main(String[] args) {
+		// we are dealing with unweighted and undirected graph
+		Graph graph = new Graph(8);
+		graph.addEdge(1, 2);
+		graph.addEdge(1, 3);
+		graph.addEdge(3, 4);
+		graph.addEdge(0, 2);
+		graph.addEdge(4, 1);
+		graph.addEdge(3, 0);
+		graph.addEdge(5, 6);
+		graph.addEdge(6, 7);
+		graph.addEdge(7, 5);
+		// let's check whether there exist any cycle in the graph or not
+		// for detecting the cycle we are going to use the bfs
+		graph.detectCycle();
+	}
+}
